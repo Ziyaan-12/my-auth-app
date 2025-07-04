894c3fab90c0a5a748c2df18defa7b890162d677
Ziyaan-12 <149665658+Ziyaan-12@users.noreply.github.com>
2025-07-04 15:55:31 +0530

    fixed prisma

--- src/lib/authOption.ts
+++ src/lib/authOption.ts
@@ -2,7 +2,7 @@ import { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { prisma } from './prisma';
+import { prisma } from '@/lib/prisma';
 import { compare } from 'bcryptjs';
 
 export const authOptions: AuthOptions = {
